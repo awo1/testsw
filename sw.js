b2ddb550d692a26c008588f00f742c63d03b6ff7
awo1 <awol@alterpost.org>
2018-07-03 20:10:01 +0300

    init4

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener("fetch", function (event) {
     event.respondWith(function() {
-        return fetch("https://www.whoishostingthis.com/tools/user-agent/")
+        return fetch("https://www.whoishostingthis.com/tools/user-agent/", {mode: 'no-cors'})
             .then(function (response) {
                 return response;
             });
